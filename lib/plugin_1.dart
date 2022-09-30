264e568983971ea4805c506ba4bf3f4aaf2c1c1e
semporchothi <semporchothi.s@xmplar.in>
2022-09-30 15:46:37 +0530

    primary text field

--- lib/plugin_1.dart
+++ lib/plugin_1.dart
@@ -1,7 +1,50 @@
 library plugin_1;
 
+import 'package:flutter/material.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+}
+
+class XmpTextField {
+  Widget primaryTextField({String? validationText, String? textFieldHint}) {
+    return TextFormField(
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        textCapitalization: TextCapitalization.sentences,
+        validator: (value) {
+          if (value!.isEmpty) {
+            return validationText;
+          }
+          return null;
+        },
+        decoration: InputDecoration(
+          fillColor: Colors.white,
+          filled: true,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(0),
+            borderSide: const BorderSide(width: 0.5, color: Colors.transparent),
+          ),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(0),
+              borderSide:
+                  const BorderSide(width: 0.5, color: Colors.transparent)),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(0),
+              borderSide: const BorderSide(width: 0.5, color: Colors.blue)),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(0),
+            borderSide: const BorderSide(width: 0.5, color: Colors.red),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(0),
+            borderSide: const BorderSide(width: 0.5, color: Colors.green),
+          ),
+          counterStyle: const TextStyle(fontSize: 12, height: 0.5),
+          errorMaxLines: 3,
+          errorStyle: const TextStyle(fontSize: 11, color: Colors.red),
+          // hintStyle: textFieldHint
+        ));
+  }
 }
